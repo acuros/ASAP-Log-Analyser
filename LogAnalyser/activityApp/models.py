40a051727d70431d3b3a6e72925fa4a29726084c
luman <namul10@gmail.com>
2012-10-01 19:42:10 +0900

    create models method

--- LogAnalyser/activityApp/models.py
+++ LogAnalyser/activityApp/models.py
@@ -1,15 +1,37 @@
 from django.db import models
 
 class Activity(models.Model):
-    time = models.DateField()
+    time = models.DateTimeField()
     return_code = models.CharField(max_length=30)
     reason = models.CharField(max_length=30)
     response_variables = models.TextField()
+
+    @classmethod
+    def load_from_dictionary(cls, dictionary):
+        activity = cls()
+        activity.time=dictionary['time']
+        activity.return_code = dictionary['Return code']
+        activity.reason = dictionary['Reason']
+        activity.response_variables = dictionary['Response variables']
+        activity.save()
+
+        key_value_class = ['GET', 'POST', 'COOKIES', 'SESSION']
+        for class_name in key_value_class:
+            eval(class_name).load_from_dictionary(activity, dictionary)
+        
 
 class KeyValueModel(models.Model):
     activity = models.ForeignKey(Activity)
     key = models.CharField(max_length=100)
     value = models.TextField()
+
+    @classmethod
+    def load_from_dictionary(cls, activity, dictionary):
+        model  = cls()
+        model .activity = activity
+        for key, value in dictionary.iteritems():
+            model.key = value
+        model.save()
 
 class GET(KeyValueModel):
     pass
